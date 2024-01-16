75017f8419178fb7a30275486611d980a917dff1
Marcos Milhomem <marcosapolo511@gmail.com>
2024-01-16 00:28:24 -0300

    nubank

--- events.js
+++ events.js
@@ -7,17 +7,17 @@ const txtHelp = document.querySelector('#txt-help')
 
 
 look.addEventListener('click', () => {
-    if(look.classList.contains('fa-eye-slash')) {
+    if (look.classList.contains('fa-eye-slash')) {
         look.classList.remove('fa-eye-slash')
         look.classList.add('fa-eye')
 
         return;
     }
-    look.classList.remove ('fa-eye')
+    look.classList.remove('fa-eye')
     look.classList.add('fa-eye-slash')
 });
 
-cpf.addEventListener ('blur', function created() {
+cpf.addEventListener('blur', function created() {
     /*    WARNING     */
     const warning = document.querySelector('#warning');
 
@@ -38,19 +38,33 @@ cpf.addEventListener ('blur', function created() {
 
 pass.addEventListener('blur', () => {
 
-    txtHelp.innerHTML= "Este campo é obrigatório";
+    txtHelp.innerHTML = "Este campo é obrigatório";
 
     txtHelp.style.color = '#D35B59'
     txtHelp.style.letterSpacing = '1px'
     inputField[1].style.borderBottom = '2px solid #D35B59'
     look.style.color = '#D35B59'
+
+    
+    return
 });
 
-pass.addEventListener('blur', () => {
-    if (pass.value.length >= 1) {
+pass.addEventListener('input', () => {
+    if (pass.value.length >= 1 && 'blur' === true) {
         txtHelp.innerHTML = 'A senha deve ter 8 dígitos ou mais'
-    } return
+    }
 });
+
+pass.addEventListener('input', () => {
+    if (pass.value.length >= 8) {
+        pass.removeEventListener('blur', pass)
+        txtHelp.innerHTML = 'Possui 8 caracteres ou mais'
+        txtHelp.style.color = 'rgb(163, 163, 163)';
+        txtHelp.style.letterSpacing = '0px';
+        look.style.color = 'rgb(163, 163, 163)';
+        inputField[1].style.borderBottom = '2px solid rgb(34, 34, 34)'
+    }
+})
 
 look.addEventListener('click', () => {
     const modePass = pass.getAttribute('type')
